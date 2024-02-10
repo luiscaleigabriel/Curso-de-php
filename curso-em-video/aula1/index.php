5f527b672084ae62819970de82a54e822896b44b
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-02-10 08:25:56 +0100

    finalizei a aula 1

---
+++ curso-em-video/aula1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+
+    <?php
+        echo "Olá, Mundo!";
+        echo "Me livrei da maldição! \u{1F379}";
+    ?>
+
+</body>
+</html>
